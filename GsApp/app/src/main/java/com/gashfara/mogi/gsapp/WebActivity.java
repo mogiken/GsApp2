f8a835b4a7d5a50a29136490e265173428bfafe1
mogiken <mogi@hugg.co.jp>
2016-02-20 14:28:28 +0900

    完成

--- GsApp/app/src/main/java/com/gashfara/mogi/gsapp/WebActivity.java
+++ GsApp/app/src/main/java/com/gashfara/mogi/gsapp/WebActivity.java
@@ -10,6 +10,9 @@ import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
 
 public class WebActivity extends ActionBarActivity {
 
@@ -34,6 +37,17 @@ public class WebActivity extends ActionBarActivity {
         //URLを表示します。
         webView.loadUrl(url);
     }
+
+    //GrowthHackで追加ここから
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Tracker t = ((VolleyApplication)getApplication()).getTracker(VolleyApplication.TrackerName.APP_TRACKER);
+        t.setScreenName(this.getClass().getSimpleName());
+        t.send(new HitBuilders.AppViewBuilder().build());
+    }
+    //GrowthHackで追加ここまで
+
     //デフォルトで作成されたメニューの関数です。未使用。
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
